faec422354251cdb23ce44354218987bd9fbdb9b
xuejingfei <xue.jingfei@immomo.com>
2021-08-26 19:36:21 +0800

    算法：从数组中找到重复的值

---
+++ app/src/test/java/com/xjf/leetcode/array/FindRepeatNum.java
@@ -0,0 +1,28 @@
+package com.xjf.leetcode.array;
+
+/**
+ * Description:
+ * Author: xuejingfei
+ * E-mail: xue.jingfei@immomo.com
+ * Date: 8/26/21 2:33 PM
+ */
+class FindRepeatNum {
+
+    int findRepeatNum(int[] nums) {
+       for(int i = 0; i <nums.length ; i++){
+           if(nums[i] == i) {
+               continue;
+           }
+           if(nums[nums[i]] == nums[i]) {
+               return nums[i];
+           }
+
+           int tmp = nums[i];
+           nums[i] = nums[tmp];
+           nums[tmp] = tmp;
+
+       }
+       return 0;
+    }
+
+}
